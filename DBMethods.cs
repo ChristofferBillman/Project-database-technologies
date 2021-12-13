09cc57df3211a1e719a7939f93f2134c8af1b48b
ChristofferBillman <christoffer.billman@gmail.com>
2021-12-13 16:00:02 +0100

    Changes in index so that Viktoria can pull.

---
+++ DBMethods.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Projekt3
+{
+	public class DBMethods
+	{
+        // Update with new connstring from Linus when he is done with DB.
+        static readonly string connString = "Server=(localdb)\\mssqllocaldb; Database=Blog; Trusted_Connection=True;";
+        
+        /// <summary>
+        /// Executes a SQL command in the database.
+        /// </summary>
+        /// <param name="sqlstring"> The SQL string.</param>
+        /// <returns> The number of rows affected. </returns>
+        public static int ExecCommand(string sqlstring)
+        {
+			SqlConnection DBConnection = new SqlConnection();
+            DBConnection.ConnectionString = connString;
+
+            SqlCommand command = new SqlCommand(sqlstring, DBConnection);
+
+            try
+            {
+                DBConnection.Open();
+                return command.ExecuteNonQuery();
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                DBConnection.Close();
+            }
+        }
+        /// <summary>
+        /// Executes a SQL query in the database.
+        /// </summary>
+        /// <param name="sqlstring"> The SQL string.  </param>
+        /// <returns> The requested data. Is accessible in ds["data"]. </returns>
+        public static DataSet ExecQuery(string sqlstring)
+        {
+            SqlConnection DBConnection = new SqlConnection();
+            DBConnection.ConnectionString = connString;
+
+            SqlCommand command = new SqlCommand(sqlstring, DBConnection);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+
+            DataSet ds = new DataSet();
+
+            try
+            {
+                DBConnection.Open();
+                dataAdapter.Fill(ds, "data");
+                return ds;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                DBConnection.Close();
+            }
+        }
+    }
+}
