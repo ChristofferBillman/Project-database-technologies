ca937ece59ce3c5114fa68be2efa31be2386d12e
ChristofferBillman <christoffer.billman@gmail.com>
2021-12-15 22:44:02 +0100

    Implemented methods in Auth.
    Only tested to a minor extent.

--- Auth.cs
+++ Auth.cs
@@ -1,30 +1,93 @@
 ﻿using Microsoft.AspNetCore.Http;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using Projekt3.Models;
 
 namespace Projekt3
 {
 	public static class Auth
 	{
 		/// <summary>
-		/// Checks if a user token is valid.
+		/// Checks if given token corresponds with the one from DB.
 		/// </summary>
 		/// <param name="token">The user token, should be avaliable in Response/Request.Cookies.</param>
 		/// <returns>True if token is valid, otherwise false.</returns>
 		/// <exception cref="System.NotImplementedException"></exception>
 		public static bool Authenticate(string token)
-		{	
-			// Fetch user from DB
-			// Compare password in token to password in DB.
-			throw new System.NotImplementedException();
+		{
+			// Get the profileId from the token.
+			int profileId = int.Parse(token.Split('_')[0]);
+
+			//Get profile from DB.
+			ProfileModel pm = ProfileMethods.SelectOne(profileId);
+
+			// If no users are found, return false.
+			if (pm == null) return false;
+
+			// If the recieved token is the same as the same
+			// as the one created from DB credentials, the token is valid.
+			if (token == (pm.ID.ToString() +"_" + pm.Password))
+			{
+				return true;
+			}
+			return false;
 		}
 		/// <summary>
-		/// Encrypts a given password.
+		/// Authenticates a user.
 		/// </summary>
-		/// <param name="password"></param>
-		/// <returns> The encrypted password.</returns>
-		/// <exception cref="System.NotImplementedException"></exception>
-		public static string Encrypt(string password)
+		/// <param name="profileId">The users profileId.</param>
+		/// <param name="password">The users password.</param>
+		/// <returns>True if password is correct, otherwise false.</returns>
+		public static bool Authenticate(int profileId, string password)
 		{
-			throw new System.NotImplementedException();
+			ProfileModel pm = ProfileMethods.SelectOne(profileId);
+
+			if (pm == null) return false;
+
+			if (Hash(password,pm.Salt) == pm.Password)
+			{
+				return true;
+			}
+			return false;
+		}
+		/// <summary>
+		/// Hashes a given password.
+		/// </summary>
+		/// <param name="password">The cleartext password.</param>
+		/// <param name="salt">The salt. Use method getRandomSalt to get a new one.</param>
+		/// <returns> The hashed password.</returns>
+		/// <exception cref="System.NotImplementedException"></exception>
+		public static string Hash(string password, string salt)
+		{
+			return Convert.ToBase64String(KeyDerivation.Pbkdf2(
+				password,
+				Convert.FromBase64String(salt),
+				KeyDerivationPrf.HMACSHA256,
+				100000,
+				256 / 8));
+		}
+		/// <summary>
+		/// Generates a random salt.
+		/// NOTE: Don't forget to set the profiles "Salt"-variabe before inserting into DB.
+		/// </summary>
+		/// <returns>The salt.</returns>
+		public static string GetRandomSalt()
+		{
+			byte[] salt = new byte[128 / 8];
+
+			RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+
+			try
+			{
+				rngCsp.GetNonZeroBytes(salt);
+			}
+			finally
+			{
+				rngCsp.Dispose();
+			}
+
+			return Convert.ToBase64String(salt);
 		}
 		/// <summary>
 		/// Performs a nullcheck on all fields in a form.
