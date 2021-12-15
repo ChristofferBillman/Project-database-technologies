2d774e37efa454e35682fc9c51c0901133156406
ChristofferBillman <christoffer.billman@gmail.com>
2021-12-15 14:25:15 +0100

    Fixed getters and setters.

--- Models/DataClasses/HobbyModel.cs
+++ Models/DataClasses/HobbyModel.cs
@@ -2,15 +2,12 @@
 {
 	public class HobbyModel
 	{
-		public int ID;
-		public string Name;
-		public char Emoji;
+		public int ID { get; }
+		public string Name { get; set; }
+		public char Emoji { get; set; }
 
 		public HobbyModel()
 		{
-			public int ID { get; }
-			public string Name { get; set;}
-			public char Emoji { get; set;}
 			
 		}
 
