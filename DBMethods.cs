cc336c6f2dcb7895bb117e0289ac38591d93334d
ChristofferBillman <christoffer.billman@gmail.com>
2021-12-21 13:44:11 +0100

    Support for both MySQL and SQL Server.
    Implemented support for both in the same file,
    should be able to merge into master branch soon.

--- DBMethods.cs
+++ DBMethods.cs
@@ -1,69 +1,119 @@
 ﻿using System;
 using System.Data;
-using MySql.Data;
+using System.Data.SqlClient;
 using MySql.Data.MySqlClient;
 
 namespace Projekt3
 {
 	public class DBMethods
 	{
-        // Update with new connstring from Linus when he is done with DB.
-        static readonly string connString = "Server=127.0.0.1;Database=Dejting;Uid=christoffer;Pwd=sten1234;";
-        /// <summary>
-        /// Executes a SQL command in the database.
-        /// </summary>
-        /// <param name="sqlstring"> The SQL string.</param>
-        /// <returns> The number of rows affected. </returns>
-        public static int ExecCommand(string sqlstring)
-        {
-			MySqlConnection DBConnection = new MySqlConnection();
-            DBConnection.ConnectionString = connString;
+		// Update with new connstring from Linus when he is done with DB.
+		static readonly string connString = Config.CONNSTRING;
+		/// <summary>
+		/// Executes a SQL command in the database.
+		/// </summary>
+		/// <param name="sqlstring"> The SQL string.</param>
+		/// <returns> The number of rows affected. </returns>
+		public static int ExecCommand(string sqlstring)
+		{
+			Console.WriteLine(Config.USEMYSQL);
+			if(Config.USEMYSQL){
+				MySqlConnection DBConnection = new MySqlConnection();
+				DBConnection.ConnectionString = connString;
 
-            MySqlCommand command = new MySqlCommand(sqlstring, DBConnection);
+				MySqlCommand command = new MySqlCommand(sqlstring, DBConnection);
 
-            try
-            {
-                DBConnection.Open();
-                return command.ExecuteNonQuery();
-            }
-            catch
-            {
-                throw;
-            }
-            finally
-            {
-                DBConnection.Close();
-            }
-        }
-        /// <summary>
-        /// Executes a SQL query in the database.
-        /// </summary>
-        /// <param name="sqlstring"> The SQL string.  </param>
-        /// <returns> The requested data. Is accessible in ds["data"]. </returns>
-        public static DataSet ExecQuery(string sqlstring)
-        {
-            MySqlConnection DBConnection = new MySqlConnection();
-            DBConnection.ConnectionString = connString;
+				try
+				{
+					DBConnection.Open();
+					return command.ExecuteNonQuery();
+				}
+				catch
+				{
+					throw;
+				}
+				finally
+				{
+					DBConnection.Close();
+				}
+			}
+			else
+			{
+				SqlConnection DBConnection = new SqlConnection();
+				DBConnection.ConnectionString = connString;
 
-            MySqlCommand command = new MySqlCommand(sqlstring, DBConnection);
-            MySqlDataAdapter dataAdapter = new MySqlDataAdapter(command);
+				SqlCommand command = new SqlCommand(sqlstring, DBConnection);
 
-            DataSet ds = new DataSet();
+				try
+				{
+					DBConnection.Open();
+					return command.ExecuteNonQuery();
+				}
+				catch
+				{
+					throw;
+				}
+				finally
+				{
+					DBConnection.Close();
+				}
+			}
+		}
+		/// <summary>
+		/// Executes a SQL query in the database.
+		/// </summary>
+		/// <param name="sqlstring"> The SQL string.  </param>
+		/// <returns> The requested data. Is accessible in ds["data"]. </returns>
+		public static DataSet ExecQuery(string sqlstring)
+		{
+			if(Config.USEMYSQL){
+				MySqlConnection DBConnection = new MySqlConnection();
+				DBConnection.ConnectionString = connString;
 
-            try
-            {
-                DBConnection.Open();
-                dataAdapter.Fill(ds, "data");
-                return ds;
-            }
-            catch
-            {
-                throw;
-            }
-            finally
-            {
-                DBConnection.Close();
-            }
-        }
-    }
+				MySqlCommand command = new MySqlCommand(sqlstring, DBConnection);
+				MySqlDataAdapter dataAdapter = new MySqlDataAdapter(command);
+
+				DataSet ds = new DataSet();
+
+				try
+				{
+					DBConnection.Open();
+					dataAdapter.Fill(ds, "data");
+					return ds;
+				}
+				catch
+				{
+					throw;
+				}
+				finally
+				{
+					DBConnection.Close();
+				}
+			}
+			else{
+				SqlConnection DBConnection = new SqlConnection();
+				DBConnection.ConnectionString = connString;
+
+				SqlCommand command = new SqlCommand(sqlstring, DBConnection);
+				SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+
+				DataSet ds = new DataSet();
+
+				try
+				{
+					DBConnection.Open();
+					dataAdapter.Fill(ds, "data");
+					return ds;
+				}
+				catch
+				{
+					throw;
+				}
+				finally
+				{
+					DBConnection.Close();
+				}
+			}
+		}
+	}
 }
