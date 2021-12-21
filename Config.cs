1089cf1801594bb2310fa57aacbe8d654c6334ab
ChristofferBillman <christoffer.billman@gmail.com>
2021-12-21 13:42:41 +0100

    Added config file.
    In this file you can set variables that are specific to your system.

---
+++ Config.cs
@@ -0,0 +1,10 @@
+namespace Projekt3
+{
+	public static class Config
+	{
+		// Your connection string for your specific system.
+		public static readonly string CONNSTRING = "Server=127.0.0.1;Database=Dejting;Uid=christoffer;Pwd=sten1234;";
+		// Set this to true if using MySQL instead of Microsoft SQL Server.
+		public static readonly bool USEMYSQL = true;
+	}
+}
